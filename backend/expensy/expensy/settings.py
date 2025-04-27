3fe74633e33f2c396305c5c7061a87c41b87bcb8
abijith-ab1 <abijithkp93@gmail.com>
2025-04-27 11:53:22 +0530

    update allowed hosts

--- backend/expensy/expensy/settings.py
+++ backend/expensy/expensy/settings.py
@@ -8,7 +8,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
